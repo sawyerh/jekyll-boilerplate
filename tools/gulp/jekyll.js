0d533a1de0f7851d5df3aab62b1aca7b2c802095
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-07-20 16:34:00 -0400

    Run with bundle exec

--- tools/gulp/jekyll.js
+++ tools/gulp/jekyll.js
@@ -1,12 +1,11 @@
-const childProcess = require('child_process');
-const jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+const run = require('gulp-run');
 
 module.exports = (gulp, shared) => {
   // Rebuild the site when changes are made
   gulp.task('jekyll:watch', done => {
     gulp.watch([
       'assets/dist/scripts/**/*.js',
-      'assets/images/**/*',
+      'images/**/*',
       '**/*.{md,html,yml}', // Templates and content
       '!node_modules/**/*',
       '!_site/**/*'
@@ -18,11 +17,8 @@ module.exports = (gulp, shared) => {
    * as `bundle exec jekyll build`.
    */
   gulp.task('jekyll:build', done => {
-    return childProcess.spawn(
-      jekyll,
-      ['build'],
-      {stdio: 'inherit'}
-    ).on('close', done);
+    const shellCommand = 'bundle exec jekyll build';
+    return gulp.src('./').pipe(run(shellCommand));
   });
 
   gulp.task('jekyll:rebuild', ['jekyll:build'], () => {
